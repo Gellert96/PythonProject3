dc5d34b368f3ed53033721a8e33a845e38a400ee
Gregory <gregory@example.com>
2026-04-07 10:42:48 +0300

    Refactor: move processing module and fix imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.processing.processing import filter_by_state, sort_by_date
+from src.processing import filter_by_state, sort_by_date
 
 print(get_mask_card_number("7000792289606361"))
 print(get_mask_account("73654108430135874305"))
