3d83cecd38ce19dd3c92613187f28884d618d34b
Gregory <gregory@example.com>
2026-05-18 11:40:15 +0300

    Fix mypy issues

--- src/readers.py
+++ src/readers.py
@@ -7,21 +7,21 @@ def read_csv_transactions(file_path: str) -> list[dict[str, Any]]:
     """
     Считывает финансовые операции из CSV-файла.
 
-    :param file_path: путь до CSV-файла
+    :param file_path: путь к CSV-файлу
     :return: список словарей с транзакциями
     """
     data = pd.read_csv(file_path)
 
-    return data.to_dict(orient="records")
+    return data.to_dict(orient="records")  # type: ignore[return-value]
 
 
 def read_excel_transactions(file_path: str) -> list[dict[str, Any]]:
     """
     Считывает финансовые операции из Excel-файла.
 
-    :param file_path: путь до Excel-файла
+    :param file_path: путь к Excel-файлу
     :return: список словарей с транзакциями
     """
     data = pd.read_excel(file_path)
 
-    return data.to_dict(orient="records")
+    return data.to_dict(orient="records")  # type: ignore[return-value]
